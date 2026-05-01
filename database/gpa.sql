8cc5cfadc5a35be9b2fce38e4c17c7ab42433bb2
Isabela Athayde <isabelaoa15@gmail.com>
2026-04-30 21:55:17 -0300

    add Calculos automatizados: MF, desempenho Semestral e CR geral.

--- database/gpa.sql
+++ database/gpa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01/05/2026 às 02:41
+-- Tempo de geração: 01/05/2026 às 02:52
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -20,11 +20,14 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `gpa`
 --
+CREATE DATABASE IF NOT EXISTS `gpa` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `gpa`;
 
 DELIMITER $$
 --
 -- Funções
 --
+DROP FUNCTION IF EXISTS `f_media_final`$$
 CREATE DEFINER=`root`@`localhost` FUNCTION `f_media_final` (`av1` DECIMAL(4,2), `av2` DECIMAL(4,2), `avf` DECIMAL(4,2)) RETURNS DECIMAL(4,2) DETERMINISTIC BEGIN
     DECLARE media_parcial DECIMAL(4,2);
 
@@ -51,26 +54,36 @@ DELIMITER ;
 -- Estrutura para tabela `aluno`
 --
 
-CREATE TABLE `aluno` (
-  `id_aluno` int(11) NOT NULL,
+DROP TABLE IF EXISTS `aluno`;
+CREATE TABLE IF NOT EXISTS `aluno` (
+  `id_aluno` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `escolaridade` varchar(50) DEFAULT NULL,
-  `CR` decimal(4,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `CR` decimal(4,2) DEFAULT NULL,
+  PRIMARY KEY (`id_aluno`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+--
+-- Despejando dados para a tabela `aluno`
+--
 
 --
 -- Estrutura para tabela `aluno_materia`
 --
 
-CREATE TABLE `aluno_materia` (
-  `id_aluno_materia` int(11) NOT NULL,
+DROP TABLE IF EXISTS `aluno_materia`;
+CREATE TABLE IF NOT EXISTS `aluno_materia` (
+  `id_aluno_materia` int(11) NOT NULL AUTO_INCREMENT,
   `id_semestre` int(11) NOT NULL,
-  `id_materia` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id_materia` int(11) NOT NULL,
+  PRIMARY KEY (`id_aluno_materia`),
+  UNIQUE KEY `id_semestre` (`id_semestre`,`id_materia`),
+  KEY `fk_am_materia` (`id_materia`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 
 -- --------------------------------------------------------
 
@@ -78,35 +91,46 @@ CREATE TABLE `aluno_materia` (
 -- Estrutura para tabela `materia`
 --
 
-CREATE TABLE `materia` (
-  `id_materia` int(11) NOT NULL,
+DROP TABLE IF EXISTS `materia`;
+CREATE TABLE IF NOT EXISTS `materia` (
+  `id_materia` int(11) NOT NULL AUTO_INCREMENT,
   `sigla` varchar(5) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `creditos` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `creditos` int(11) NOT NULL,
+  PRIMARY KEY (`id_materia`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 
 --
 -- Estrutura para tabela `notas`
 --
 
-CREATE TABLE `notas` (
-  `id_nota` int(11) NOT NULL,
+DROP TABLE IF EXISTS `notas`;
+CREATE TABLE IF NOT EXISTS `notas` (
+  `id_nota` int(11) NOT NULL AUTO_INCREMENT,
   `id_aluno_materia` int(11) NOT NULL,
   `av1` decimal(4,2) DEFAULT NULL,
   `av2` decimal(4,2) DEFAULT NULL,
   `avf` decimal(4,2) DEFAULT NULL,
-  `mf` decimal(4,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `mf` decimal(4,2) DEFAULT NULL,
+  PRIMARY KEY (`id_nota`),
+  UNIQUE KEY `id_aluno_materia` (`id_aluno_materia`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 
 --
 -- Acionadores `notas`
 --
+DROP TRIGGER IF EXISTS `trg_calcular_mf`;
 DELIMITER $$
 CREATE TRIGGER `trg_calcular_mf` BEFORE INSERT ON `notas` FOR EACH ROW BEGIN
     SET NEW.mf = f_media_final(NEW.av1, NEW.av2, NEW.avf);
 END
 $$
 DELIMITER ;
+DROP TRIGGER IF EXISTS `trg_insert_desempenho`;
 DELIMITER $$
 CREATE TRIGGER `trg_insert_desempenho` AFTER INSERT ON `notas` FOR EACH ROW BEGIN
     UPDATE semestre s
@@ -122,6 +146,7 @@ CREATE TRIGGER `trg_insert_desempenho` AFTER INSERT ON `notas` FOR EACH ROW BEGI
 END
 $$
 DELIMITER ;
+DROP TRIGGER IF EXISTS `trg_update_desempenho`;
 DELIMITER $$
 CREATE TRIGGER `trg_update_desempenho` AFTER UPDATE ON `notas` FOR EACH ROW BEGIN
     IF (OLD.mf IS NULL AND NEW.mf IS NOT NULL)
@@ -143,6 +168,7 @@ CREATE TRIGGER `trg_update_desempenho` AFTER UPDATE ON `notas` FOR EACH ROW BEGI
 END
 $$
 DELIMITER ;
+DROP TRIGGER IF EXISTS `trg_update_mf`;
 DELIMITER $$
 CREATE TRIGGER `trg_update_mf` BEFORE UPDATE ON `notas` FOR EACH ROW BEGIN
     SET NEW.mf = f_media_final(NEW.av1, NEW.av2, NEW.avf);
@@ -156,20 +182,20 @@ DELIMITER ;
 -- Estrutura para tabela `semestre`
 --
 
-CREATE TABLE `semestre` (
-  `id_semestre` int(11) NOT NULL,
+DROP TABLE IF EXISTS `semestre`;
+CREATE TABLE IF NOT EXISTS `semestre` (
+  `id_semestre` int(11) NOT NULL AUTO_INCREMENT,
   `id_aluno` int(11) NOT NULL,
   `titulo` varchar(6) NOT NULL,
-  `desempenho` decimal(4,2) NOT NULL DEFAULT 0.00
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `desempenho` decimal(4,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`id_semestre`),
+  UNIQUE KEY `id_aluno` (`id_aluno`,`titulo`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `semestre`
---
 
---
 -- Acionadores `semestre`
 --
+DROP TRIGGER IF EXISTS `trg_update_cr`;
 DELIMITER $$
 CREATE TRIGGER `trg_update_cr` AFTER UPDATE ON `semestre` FOR EACH ROW BEGIN
     UPDATE aluno a
@@ -194,19 +220,23 @@ DELIMITER ;
 -- Estrutura para tabela `trabalhos`
 --
 
-CREATE TABLE `trabalhos` (
-  `id_trabalho` int(11) NOT NULL,
+DROP TABLE IF EXISTS `trabalhos`;
+CREATE TABLE IF NOT EXISTS `trabalhos` (
+  `id_trabalho` int(11) NOT NULL AUTO_INCREMENT,
   `id_aluno_materia` int(11) NOT NULL,
   `titulo` varchar(100) DEFAULT NULL,
   `data_entrega_prevista` date DEFAULT NULL,
   `data_entrega_aluno` date DEFAULT NULL,
-  `situacao` enum('atribuida','pendente','entregue') DEFAULT 'atribuida'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `situacao` enum('atribuida','pendente','entregue') DEFAULT 'atribuida',
+  PRIMARY KEY (`id_trabalho`),
+  KEY `fk_trabalhos_am` (`id_aluno_materia`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 --
 -- Acionadores `trabalhos`
 --
+DROP TRIGGER IF EXISTS `atualizar_situacao_trabalho`;
 DELIMITER $$
 CREATE TRIGGER `atualizar_situacao_trabalho` BEFORE UPDATE ON `trabalhos` FOR EACH ROW BEGIN
     IF NEW.data_entrega_aluno IS NULL 
@@ -220,92 +250,6 @@ CREATE TRIGGER `atualizar_situacao_trabalho` BEFORE UPDATE ON `trabalhos` FOR EA
 END
 $$
 DELIMITER ;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `aluno`
---
-ALTER TABLE `aluno`
-  ADD PRIMARY KEY (`id_aluno`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Índices de tabela `aluno_materia`
---
-ALTER TABLE `aluno_materia`
-  ADD PRIMARY KEY (`id_aluno_materia`),
-  ADD UNIQUE KEY `id_semestre` (`id_semestre`,`id_materia`),
-  ADD KEY `fk_am_materia` (`id_materia`);
-
---
--- Índices de tabela `materia`
---
-ALTER TABLE `materia`
-  ADD PRIMARY KEY (`id_materia`);
-
---
--- Índices de tabela `notas`
---
-ALTER TABLE `notas`
-  ADD PRIMARY KEY (`id_nota`),
-  ADD UNIQUE KEY `id_aluno_materia` (`id_aluno_materia`);
-
---
--- Índices de tabela `semestre`
---
-ALTER TABLE `semestre`
-  ADD PRIMARY KEY (`id_semestre`),
-  ADD UNIQUE KEY `id_aluno` (`id_aluno`,`titulo`);
-
---
--- Índices de tabela `trabalhos`
---
-ALTER TABLE `trabalhos`
-  ADD PRIMARY KEY (`id_trabalho`),
-  ADD KEY `fk_trabalhos_am` (`id_aluno_materia`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `aluno`
---
-ALTER TABLE `aluno`
-  MODIFY `id_aluno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de tabela `aluno_materia`
---
-ALTER TABLE `aluno_materia`
-  MODIFY `id_aluno_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT de tabela `materia`
---
-ALTER TABLE `materia`
-  MODIFY `id_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT de tabela `notas`
---
-ALTER TABLE `notas`
-  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT de tabela `semestre`
---
-ALTER TABLE `semestre`
-  MODIFY `id_semestre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de tabela `trabalhos`
---
-ALTER TABLE `trabalhos`
-  MODIFY `id_trabalho` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para tabelas despejadas
