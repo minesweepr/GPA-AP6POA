983984a16f41442c1a6f60328ff198987f14515e
Isabela Athayde <isabelaoa15@gmail.com>
2026-05-07 20:30:21 -0300

    refatoração: ajustes em notas.jsp, estiloTabela.css e model Notas

--- src/main/java/model/Notas.java
+++ src/main/java/model/Notas.java
@@ -3,10 +3,10 @@ package model;
 public class Notas{
     private int idNota;
     private AlunoMateria alunoMateria;
-    private double av1;
-    private double av2;
-    private double avf;
-    private double mf;
+    private Double av1;
+    private Double av2;
+    private Double avf;
+    private Double mf;
 
     public int getIdNota(){return idNota;}
     public void setIdNota(int idNota){this.idNota = idNota;}
@@ -14,15 +14,15 @@ public class Notas{
     public AlunoMateria getAlunoMateria(){return alunoMateria;}
     public void setAlunoMateria(AlunoMateria alunoMateria){this.alunoMateria = alunoMateria;}
 
-    public double getAv1(){return av1;}
-    public void setAv1(double av1){this.av1 = av1;}
+    public Double getAv1(){return av1;}
+    public void setAv1(Double av1){this.av1 = av1;}
 
-    public double getAv2(){return av2;}
-    public void setAv2(double av2){this.av2 = av2;}
+    public Double getAv2(){return av2;}
+    public void setAv2(Double av2){this.av2 = av2;}
 
-    public double getAvf(){return avf;}
-    public void setAvf(double avf){this.avf = avf;}
+    public Double getAvf(){return avf;}
+    public void setAvf(Double avf){this.avf = avf;}
 
-    public double getMf(){return mf;}
-    public void setMf(double mf){this.mf = mf;}
+    public Double getMf(){return mf;}
+    public void setMf(Double mf){this.mf = mf;}
 }
